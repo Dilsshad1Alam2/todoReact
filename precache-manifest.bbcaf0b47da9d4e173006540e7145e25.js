b6eb1da167d55a0fa0ee89d17db277ba4047296f
Dilshad <dilshad.alam3215@gmail.com>
2020-05-12 13:56:14 +0530

    Updates

---
+++ precache-manifest.bbcaf0b47da9d4e173006540e7145e25.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d58b41646d77e9aa22f555b278a4285",
+    "url": "/todoReact/index.html"
+  },
+  {
+    "revision": "b2bce7d6b9ed75ef07dc",
+    "url": "/todoReact/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "14440eee84cb1e4f2c90",
+    "url": "/todoReact/static/js/2.57ccfd3b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todoReact/static/js/2.57ccfd3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2bce7d6b9ed75ef07dc",
+    "url": "/todoReact/static/js/main.83b40b2e.chunk.js"
+  },
+  {
+    "revision": "f12e0afa3cb251589d75",
+    "url": "/todoReact/static/js/runtime-main.7add8510.js"
+  }
+]);
